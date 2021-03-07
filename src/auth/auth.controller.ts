3f6112fef513d90ca89b14fe55ab5641663f10b2
aisen <aisenkim15@gmail.com>
2021-03-07 23:40:53 +0900

    added signup auth with validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import {AuthService} from './auth.service';
-import {AuthCredentialsDto} from './dto/auth-credential.dto';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    constructor(
-        private authService: AuthService
-    ) {
-
-    }
-    
-    @Post('/signup')
-    signUp(@Body() authCredentialDto: AuthCredentialsDto): Promise<void> {
-        return this.authService.signUp(authCredentialDto);
-    }
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
+  }
 }
