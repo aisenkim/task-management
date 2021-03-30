e32b8546c872082dd87575ff6baaa0caeb8f2d5e
Aisen Kim <aisenkim15@gmail.com>
2021-03-30 13:55:12 +0900

    added config folder at root and added configuration files

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,16 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
 
+const jwtConfig = config.get('jwt');
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'topSecret51', // in real app, provide complicated secret
+      secret: process.env.JWT_SECRET || jwtConfig.secret, // in real app, provide complicated secret
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: jwtConfig.expiresIn,
       },
     }),
     TypeOrmModule.forFeature([UserRepository]),
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  // knows if it's development or production through NODE_ENV
+  // NODE_ENV is null by default and config assigns development settings by default
+  const serverConfig = config.get('server');
+
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
+
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
